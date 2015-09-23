f89db00b127492b84152f8a78a916e192bd796b3
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-09-23 19:48:25 +0900

    Rename resources to the easy to handle

--- UIKit/src/UIImage+UIPrivate.m
+++ UIKit/src/UIImage+UIPrivate.m
@@ -71,82 +71,82 @@ NSMutableDictionary *imageCache = nil;
 
 + (UIImage *)_backButtonImage
 {
-    return [self _frameworkImageWithName:@"<UINavigationBar> back.png" leftCapWidth:18 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UINavigationBar-back.png" leftCapWidth:18 topCapHeight:0];
 }
 
 + (UIImage *)_highlightedBackButtonImage
 {
-    return [self _frameworkImageWithName:@"<UINavigationBar> back-highlighted.png" leftCapWidth:18 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UINavigationBar-back-highlighted.png" leftCapWidth:18 topCapHeight:0];
 }
 
 + (UIImage *)_toolbarButtonImage
 {
-    return [self _frameworkImageWithName:@"<UIToolbar> button.png" leftCapWidth:6 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIToolbar-button.png" leftCapWidth:6 topCapHeight:0];
 }
 
 + (UIImage *)_highlightedToolbarButtonImage
 {
-    return [self _frameworkImageWithName:@"<UIToolbar> button-highlighted.png" leftCapWidth:6 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIToolbar-button-highlighted.png" leftCapWidth:6 topCapHeight:0];
 }
 
 + (UIImage *)_leftPopoverArrowImage
 {
-    return [self _frameworkImageWithName:@"<UIPopoverView> arrow-left.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIPopoverView-arrow-left.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_rightPopoverArrowImage
 {
-    return [self _frameworkImageWithName:@"<UIPopoverView> arrow-right.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIPopoverView-arrow-right.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_topPopoverArrowImage
 {
-    return [self _frameworkImageWithName:@"<UIPopoverView> arrow-top.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIPopoverView-arrow-top.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_bottomPopoverArrowImage
 {
-    return [self _frameworkImageWithName:@"<UIPopoverView> arrow-bottom.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIPopoverView-arrow-bottom.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_popoverBackgroundImage
 {
-    return [self _frameworkImageWithName:@"<UIPopoverView> background.png" leftCapWidth:23 topCapHeight:23];
+    return [self _frameworkImageWithName:@"UIPopoverView-background.png" leftCapWidth:23 topCapHeight:23];
 }
 
 + (UIImage *)_roundedRectButtonImage
 {
-    return [self _frameworkImageWithName:@"<UIRoundedRectButton> normal.png" leftCapWidth:12 topCapHeight:9];
+    return [self _frameworkImageWithName:@"UIRoundedRectButton-normal.png" leftCapWidth:12 topCapHeight:9];
 }
 
 + (UIImage *)_highlightedRoundedRectButtonImage
 {
-    return [self _frameworkImageWithName:@"<UIRoundedRectButton> highlighted.png" leftCapWidth:12 topCapHeight:9];
+    return [self _frameworkImageWithName:@"UIRoundedRectButton-highlighted.png" leftCapWidth:12 topCapHeight:9];
 }
 
 + (UIImage *)_windowResizeGrabberImage
 {
-    return [self _frameworkImageWithName:@"<UIScreen> grabber.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIScreen-grabber.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_buttonBarSystemItemAdd
 {
-    return [self _frameworkImageWithName:@"<UIBarButtonSystemItem> add.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIBarButtonSystemItem-add.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_buttonBarSystemItemReply
 {
-    return [self _frameworkImageWithName:@"<UIBarButtonSystemItem> reply.png" leftCapWidth:0 topCapHeight:0];
+    return [self _frameworkImageWithName:@"UIBarButtonSystemItem-reply.png" leftCapWidth:0 topCapHeight:0];
 }
 
 + (UIImage *)_tabBarBackgroundImage
 {
-  return [self _frameworkImageWithName:@"<UITabBar> background.png" leftCapWidth:6 topCapHeight:0];
+  return [self _frameworkImageWithName:@"UITabBar-background.png" leftCapWidth:6 topCapHeight:0];
 }
 
 + (UIImage *)_tabBarItemImage
 {
-  return [self _frameworkImageWithName:@"<UITabBar> item.png" leftCapWidth:8 topCapHeight:0];
+  return [self _frameworkImageWithName:@"UITabBar-item.png" leftCapWidth:8 topCapHeight:0];
 }
 
 - (id)_initWithRepresentations:(NSArray *)reps
