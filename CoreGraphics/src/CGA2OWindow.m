1df13ccb4be9c8534234e49631811eb282370937
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-10-16 16:11:23 -1000

    Implement CGInsetRectForNativeWindowBorder / CGOutsetRectForNativeWindowBorder

---
+++ CoreGraphics/src/CGA2OWindow.m
@@ -0,0 +1,9 @@
+#import <CoreGraphics/CGWindow.h>
+
+CGRect CGInsetRectForNativeWindowBorder(CGRect frame, unsigned styleMask) {
+    return frame;
+}
+CGRect CGOutsetRectForNativeWindowBorder(CGRect frame, unsigned styleMask) {
+    return frame;
+}
+
