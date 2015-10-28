9eab89be6f4872c0f924c911a24d62422521370a
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-10-28 21:18:08 +0900

    Implement CADisplayLink

--- QuartzCore/src/CADisplayLink.m
+++ QuartzCore/src/CADisplayLink.m
@@ -1,4 +1,29 @@
 #import <QuartzCore/CADisplayLink.h>
 
-@implementation CADisplayLink
+@implementation CADisplayLink {
+    id _target;
+    SEL _sel;
+}
+
++ (CADisplayLink *)displayLinkWithTarget:(id)target selector:(SEL)sel {
+    return [[CADisplayLink alloc] initWithTarget:target selector:sel];
+}
+
+- (CADisplayLink *)initWithTarget:(id)target selector:(SEL)sel {
+   _target = target;
+   _sel = sel;
+}
+
+- (void)addToRunLoop:(NSRunLoop *)runloop forMode:(NSString *)mode {
+#warning TODO better implementation
+    [NSTimer scheduledTimerWithTimeInterval:1.0f/60 target:_target selector:_sel userInfo:nil repeats:YES];
+}
+
+- (void)removeFromRunLoop:(NSRunLoop *)runloop forMode:(NSString *)mode {
+    NSLog(@"%s not implemented", __FUNCTION__);
+}
+
+- (void)invalidate {
+    NSLog(@"%s not implemented", __FUNCTION__);
+}
 @end
