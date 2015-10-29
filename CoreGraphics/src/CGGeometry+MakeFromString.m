0b050d89b5a4d8348039be5bca447c0199392aa3
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-10-29 15:34:00 +0900

    Add CGMake*FromString (from WinObjC)

---
+++ CoreGraphics/src/CGGeometry+MakeFromString.m
@@ -0,0 +1,41 @@
+//******************************************************************************
+//
+// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+#import <CoreGraphics/CGGeometry.h>
+#import <Foundation/NSString.h>
+
+CGPoint CGPointFromString(NSString *strPt) {
+    CGPoint ret;
+
+    char* str = (char*)[strPt UTF8String];
+    sscanf(str, "{%f, %f}", &ret.x, &ret.y);
+    return ret;
+}
+
+CGSize CGSizeFromString(NSString *strSize) {
+    CGSize ret;
+
+    char* str = (char*)[strSize UTF8String];
+    sscanf(str, "{%f, %f}", &ret.width, &ret.height);
+    return ret;
+}
+
+CGRect CGRectFromString(NSString *strRect) {
+    CGRect ret;
+
+    char* str = (char*)[strRect UTF8String];
+    sscanf(str, "{{%f, %f}, {%f, %f}}", &ret.origin.x, &ret.origin.y, &ret.size.width, &ret.size.height);
+    return ret;
+}
