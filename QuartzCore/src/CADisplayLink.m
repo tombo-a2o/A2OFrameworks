9a952a35589b41b33663af820a3ffdcfa3f487e6
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-11-07 15:33:53 +0900

    Invalidate timer

--- QuartzCore/src/CADisplayLink.m
+++ QuartzCore/src/CADisplayLink.m
@@ -5,7 +5,7 @@
     id _target;
     SEL _sel;
     CFTimeInterval _timestamp;
-    NSTimer *_timer;
+    dispatch_source_t _source;
 }
 
 + (CADisplayLink *)displayLinkWithTarget:(id)target selector:(SEL)sel {
@@ -21,22 +21,21 @@
 }
 
 - (void)addToRunLoop:(NSRunLoop *)runloop forMode:(NSString *)mode {
-    dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 1<<10, dispatch_get_current_queue());
-    dispatch_source_set_timer(source, 0, 0, 0);
-    dispatch_source_set_event_handler(source, ^{
+    _source = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 1<<10, dispatch_get_current_queue());
+    dispatch_source_set_timer(_source, 0, 0, 0);
+    dispatch_source_set_event_handler(_source, ^{
         [_target performSelector:_sel withObject:self];
         _timestamp = CFAbsoluteTimeGetCurrent();
     });
-    dispatch_resume(source);
+    dispatch_resume(_source);
 }
 
 - (void)removeFromRunLoop:(NSRunLoop *)runloop forMode:(NSString *)mode {
-    [_timer invalidate];
+    dispatch_source_cancel(_source);
 }
 
 - (void)invalidate {
-    [_timer invalidate];
-    [_timer release];
+    dispatch_source_cancel(_source);
     [_target release];
     _target = nil;
     _sel = nil;
