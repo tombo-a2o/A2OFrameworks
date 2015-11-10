b93f987f7c1a0660d6f7ec7c0447e89bc93935ae
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-11-10 19:34:39 +0900

    Check layout before rederging

--- QuartzCore/src/CALayer.m
+++ QuartzCore/src/CALayer.m
@@ -334,6 +334,8 @@ NSString * const kCATransition = @"transition";
 }
 
 -(void)displayIfNeeded {
+    [self layoutIfNeeded];
+
     if(_needsDisplay) {
         [self display];
         [self _setTextureId:0];
