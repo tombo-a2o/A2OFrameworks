5659dbbded0e8a4a046f9752f200a878264f2ba4
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-12-02 03:49:55 +0900

    Revert NSFontAttributeName removal

---
+++ AppKit/src/NSAttributedString.m
@@ -0,0 +1,33 @@
+/* Copyright (c) 2006-2009 Christopher J. W. Lloyd <cjwl@objc.net>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+#import <AppKit/NSAttributedString.h>
+
+NSString * const NSFontAttributeName=@"NSFontAttributeName"; 
+NSString * const NSParagraphStyleAttributeName=@"NSParagraphStyleAttributeName";
+NSString * const NSForegroundColorAttributeName=@"NSForegroundColorAttributeName";
+NSString * const NSBackgroundColorAttributeName=@"NSBackgroundColorAttributeName";
+NSString * const NSUnderlineStyleAttributeName=@"NSUnderlineStyleAttributeName";
+NSString * const NSUnderlineColorAttributeName=@"NSUnderlineColorAttributeName";
+NSString * const NSAttachmentAttributeName=@"NSAttachmentAttributeName";
+NSString * const NSKernAttributeName=@"NSKernAttributeName";
+NSString * const NSLigatureAttributeName=@"NSLigatureAttributeName";
+NSString * const NSStrikethroughStyleAttributeName=@"NSStrikethroughStyleAttributeName";
+NSString * const NSStrikethroughColorAttributeName=@"NSStrikethroughColorAttributeName";
+NSString * const NSObliquenessAttributeName=@"NSObliquenessAttributeName";
+NSString * const NSStrokeWidthAttributeName=@"NSStrokeWidthAttributeName";
+NSString * const NSStrokeColorAttributeName=@"NSStrokeColorAttributeName";
+NSString * const NSBaselineOffsetAttributeName=@"NSBaselineOffsetAttributeName";
+NSString * const NSSuperscriptAttributeName=@"NSSuperscriptAttributeName";
+NSString * const NSLinkAttributeName=@"NSLinkAttributeName";
+NSString * const NSShadowAttributeName=@"NSShadowAttributeName";
+NSString * const NSExpansionAttributeName=@"NSExpansionAttributeName";
+NSString * const NSCursorAttributeName=@"NSCursorAttributeName";
+NSString * const NSToolTipAttributeName=@"NSToolTipAttributeName";
+NSString * const NSBackgroundColorDocumentAttribute=@"NSBackgroundColorDocumentAttribute";
+NSString * const NSSpellingStateAttributeName=@"NSSpellingStateAttributeName"; // temporary attribute
