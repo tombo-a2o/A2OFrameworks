802a17210ae0e5eb00eb9f24e9de2a1297f09bd3
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-04-06 12:38:58 -0700

    Remove return statement which prevents calling super

--- Onyx2D/src/O2GraphicsState.m
+++ Onyx2D/src/O2GraphicsState.m
@@ -73,7 +73,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     O2ColorRelease(_shadowColor);
     O2GaussianKernelRelease((O2GaussianKernelRef)_shadowKernel);
     NSDeallocateObject(self);
-    return;
     [super dealloc];
 }
 
