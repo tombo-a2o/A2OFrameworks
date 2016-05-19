e2828cbdc47faca14cc73e9f18bb651abf05dc45
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-05-20 01:40:32 +0900

    Use emscripten_get_now to avoid tempRet unexpected change bug

--- QuartzCore/src/CABase.m
+++ QuartzCore/src/CABase.m
@@ -1,22 +1,6 @@
 #import <QuartzCore/CABase.h>
-#import <Foundation/NSString.h>
-#import <mach/mach_time.h>
-
-static double conversionFactor(){
-   struct mach_timebase_info timebase;
-   kern_return_t error;
-   
-   if((mach_timebase_info(&timebase))!=KERN_SUCCESS){
-    NSLog(@"mach_timebase_info returned %d",error);
-    return 1;
-   }
-
-   return  0.000000001 * ((double)timebase.numer/(double)timebase.denom);
-}
+#import <emscripten.h>
 
 CFTimeInterval CACurrentMediaTime(void) {
-   uint64_t value=mach_absolute_time();
-
-   return ((double) value)*conversionFactor();
+   return emscripten_get_now()/1000000;
 }
-
