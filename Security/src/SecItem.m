e143c2302968b13f7368a90b18e0de994af6a8de
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-10-12 14:23:04 +0900

    WIP: implementation of SecItemAdd

--- Security/src/SecItem.m
+++ Security/src/SecItem.m
@@ -2,14 +2,42 @@
 #import <stdio.h>
 #import <Foundation/Foundation.h>
 
-OSStatus SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *result)
+OSStatus SecItemCopyMatching(CFDictionaryRef _query, CFTypeRef *_result)
 {
-    fprintf(stderr, "%s not implemented\n", __FUNCTION__);
-    *result = [[NSDictionary alloc] init];
+    NSDictionary *query = _query;
+    
+    // currently supports only kSecClass=kSecClassGenericPassword and kSecReturnAttributes=true 
+    NSObject *secClass = [query objectForKey:kSecClass];
+    assert(secClass == kSecClassGenericPassword);
+    NSNumber *returnData = [query objectForKey:kSecReturnData];
+    NSNumber *returnAttributes = [query objectForKey:kSecReturnAttributes];
+    NSNumber *returnRef = [query objectForKey:kSecReturnRef];
+    NSNumber *returnPersistentRef = [query objectForKey:kSecReturnPersistentRef];
+    assert(![returnData boolValue]);
+    assert([returnAttributes boolValue]);
+    assert(![returnRef boolValue]);
+    assert(![returnPersistentRef boolValue]);
+    
+    NSString *service = [query objectForKey:kSecAttrService];
+    NSString *account = [query objectForKey:kSecAttrAccount];
+    NSLog(@"%s query: %@", __FUNCTION__, query);
+    
+    
+    NSDictionary *result = [[NSDictionary alloc] init];
+    *_result = result;
     return errSecItemNotFound;
 }
 
-OSStatus SecItemAdd(CFDictionaryRef attributes, CFTypeRef *result)
+OSStatus SecItemAdd(CFDictionaryRef _attributes, CFTypeRef *result)
 {
-    return 1;
+    NSDictionary *attributes = _attributes;
+    
+    // currently supports only kSecClass=kSecClassGenericPassword
+    NSObject *secClass = [attributes objectForKey:kSecClass];
+    assert(secClass == kSecClassGenericPassword);
+    
+    NSString *service = [attributes objectForKey:kSecAttrService];
+    NSString *account = [attributes objectForKey:kSecAttrAccount];
+    NSLog(@"%s attributes: %@", __FUNCTION__, attributes);
+    return 0;
 }
