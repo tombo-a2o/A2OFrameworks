229576cc5e5ced3bee9baaa2b873e457f07ade79
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-10-19 14:30:15 +0900

    Remove duplicate runtime

--- objc/dyld.h
+++
@@ -1,4 +0,0 @@
-#import <stdint.h>
-#import <objc/objc-export.h>
-
-OBJC_EXPORT int _NSGetExecutablePath(char *buf, uint32_t *bufsize);
--- objc/objc_malloc.h
+++
@@ -1,6 +0,0 @@
-#import <stdlib.h>
-
-void *objc_malloc(size_t size);
-void *objc_realloc(void *ptr, size_t size);
-void *objc_calloc(size_t count, size_t size);
-void objc_free(void *ptr);
