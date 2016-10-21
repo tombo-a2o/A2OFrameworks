f74893ef02ffc821200b75dd8326d0ed564dab67
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-10-21 18:16:26 +0900

    Add constants

--- MobileCoreServices/include/MobileCoreServices/UTType.h
+++ MobileCoreServices/include/MobileCoreServices/UTType.h
@@ -5,6 +5,18 @@ extern const CFStringRef kUTTagClassMIMEType;
 extern const CFStringRef kUTTagClassNSPboardType;
 extern const CFStringRef kUTTagClassOSType;
 
+extern const CFStringRef kUTTypeImage;
+extern const CFStringRef kUTTypeJPEG;
+extern const CFStringRef kUTTypeJPEG2000;
+extern const CFStringRef kUTTypeTIFF;
+extern const CFStringRef kUTTypePICT;
+extern const CFStringRef kUTTypeGIF;
+extern const CFStringRef kUTTypePNG;
+extern const CFStringRef kUTTypeQuickTimeImage;
+extern const CFStringRef kUTTypeAppleICNS;
+extern const CFStringRef kUTTypeBMP;
+extern const CFStringRef kUTTypeICO;
+
 CFStringRef UTTypeCreatePreferredIdentifierForTag ( CFStringRef inTagClass, CFStringRef inTag, CFStringRef inConformingToUTI );
 CFArrayRef UTTypeCreateAllIdentifiersForTag ( CFStringRef inTagClass, CFStringRef inTag, CFStringRef inConformingToUTI );
 CFStringRef UTTypeCopyPreferredTagWithClass ( CFStringRef inUTI, CFStringRef inTagClass );
--- MobileCoreServices/src/UTType.c
+++ MobileCoreServices/src/UTType.c
@@ -1,5 +1,22 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 
+const CFStringRef kUTTagClassFilenameExtension = CFSTR("filenameExtension");
+const CFStringRef kUTTagClassMIMEType = CFSTR("mimeType");
+const CFStringRef kUTTagClassNSPboardType = CFSTR("");
+const CFStringRef kUTTagClassOSType = CFSTR("");
+
+const CFStringRef kUTTypeImage = CFSTR("image");
+const CFStringRef kUTTypeJPEG = CFSTR("image/jpeg");
+const CFStringRef kUTTypeJPEG2000 = CFSTR("image/jpeng2000");
+const CFStringRef kUTTypeTIFF = CFSTR("image/tiff");
+const CFStringRef kUTTypePICT = CFSTR("image/pict");
+const CFStringRef kUTTypeGIF = CFSTR("image/gif");
+const CFStringRef kUTTypePNG = CFSTR("image/png");
+const CFStringRef kUTTypeQuickTimeImage = CFSTR("image/qif");
+const CFStringRef kUTTypeAppleICNS = CFSTR("image/icns");
+const CFStringRef kUTTypeBMP = CFSTR("image/bmp");
+const CFStringRef kUTTypeICO = CFSTR("image/ico");
+
 CFStringRef UTTypeCreatePreferredIdentifierForTag ( CFStringRef inTagClass, CFStringRef inTag, CFStringRef inConformingToUTI )
 {
     fprintf(stderr, "%s not implemented\n", __FUNCTION__);
