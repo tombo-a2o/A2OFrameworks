23d46e4d4e2e9c0e69a07276cf9255389b7131e1
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-11-04 13:23:51 +0900

    Extern C on C++

--- QuartzCore/include/QuartzCore/CABase.h
+++ QuartzCore/include/QuartzCore/CABase.h
@@ -1,5 +1,9 @@
 #import <CoreFoundation/CoreFoundation.h>
 
+#if defined(__cplusplus)
+#define CA_EXPORT extern "C"
+#else
 #define CA_EXPORT extern
+#endif
 
 CA_EXPORT CFTimeInterval CACurrentMediaTime(void);
