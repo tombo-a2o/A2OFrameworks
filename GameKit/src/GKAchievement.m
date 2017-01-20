efd437f7308bf98be01036da03bfb2ff8e19fc36
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-01-20 21:38:13 +0900

    Implement stubs

--- GameKit/src/GKAchievement.m
+++ GameKit/src/GKAchievement.m
@@ -1,4 +1,31 @@
 #import <GameKit/GKAchievement.h>
 
 @implementation GKAchievement
++ (void)loadAchievementsWithCompletionHandler:(void (^)(NSArray *achievements, NSError *error))completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+- (instancetype)initWithIdentifier:(NSString *)identifier
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+//- (instancetype)initWithIdentifier:(NSString *)identifier player:(GKPlayer *)player;
+
++ (void)reportAchievements:(NSArray *)achievements withCompletionHandler:(void (^)(NSError *error))completionHandler;
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
++ (void)reportAchievements:(NSArray *)achievements withEligibleChallenges:(NSArray *)challenges withCompletionHandler:(void (^)(NSError *error))completionHandler;
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+- (void)reportAchievementWithCompletionHandler:(void (^)(NSError *error))completionHandler;
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
++ (void)resetAchievementsWithCompletionHandler:(void (^)(NSError *error))completionHandler;
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
 @end
