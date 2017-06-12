a3aa63ef3e158282fd0b5146f0f4acbc653fa0fb
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-12 13:05:20 +0900

    Add missing properties

--- AdSupport/include/AdSupport/ASIdentifierManager.h
+++ AdSupport/include/AdSupport/ASIdentifierManager.h
@@ -2,4 +2,6 @@
 
 @interface ASIdentifierManager : NSObject
 @property(nonatomic, readonly) NSUUID *advertisingIdentifier;
+@property(nonatomic, readonly, getter=isAdvertisingTrackingEnabled) BOOL advertisingTrackingEnabled;
++ (ASIdentifierManager *)sharedManager;
 @end
