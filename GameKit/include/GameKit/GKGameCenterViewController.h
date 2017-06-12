fade83a1d2945a178ea7748ad2f253c26d0bee14
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-12 12:46:50 +0900

    Add missing properties

--- GameKit/include/GameKit/GKGameCenterViewController.h
+++ GameKit/include/GameKit/GKGameCenterViewController.h
@@ -6,5 +6,15 @@
 - (void)gameCenterViewControllerDidFinish:(GKGameCenterViewController *)gameCenterViewController;
 @end
 
+typedef NS_ENUM(NSInteger, GKGameCenterViewControllerState) {
+    GKGameCenterViewControllerStateDefault = -1,
+    GKGameCenterViewControllerStateLeaderboards,
+    GKGameCenterViewControllerStateAchievements,
+    GKGameCenterViewControllerStateChallenges,
+};
+
 @interface GKGameCenterViewController : UIViewController
+@property(assign, nonatomic) id<GKGameCenterControllerDelegate> gameCenterDelegate;
+@property(assign, nonatomic) GKGameCenterViewControllerState viewState;
+@property(nonatomic, retain) NSString *leaderboardIdentifier;
 @end
