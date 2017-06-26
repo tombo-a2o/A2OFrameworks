ba9c1b55469ae9ad15ccf23e95b9b1ed3230dc1f
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-26 17:39:02 +0900

    Add stubs

--- CoreMotion/src/CMMotionManager.m
+++ CoreMotion/src/CMMotionManager.m
@@ -1,4 +1,19 @@
 #import <CoreMotion/CoreMotion.h>
 
 @implementation CMMotionManager
+- (void)startAccelerometerUpdatesToQueue:(NSOperationQueue *)queue withHandler:(CMAccelerometerHandler)handler
+{
+  NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)startAccelerometerUpdates;
+{
+  NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)stopAccelerometerUpdates;
+{
+  NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
 @end
