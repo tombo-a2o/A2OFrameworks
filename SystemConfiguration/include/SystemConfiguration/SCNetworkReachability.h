61ddba08c6f99107c04f07aac21acbbb57babd9f
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-26 23:38:39 +0900

    Remove TARGET_OS_EMSCRIPTEN

--- SystemConfiguration/include/SystemConfiguration/SCNetworkReachability.h
+++ SystemConfiguration/include/SystemConfiguration/SCNetworkReachability.h
@@ -2,14 +2,14 @@
  * Copyright (c) 2003-2005, 2008-2010 Apple Inc. All rights reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
  * compliance with the License. Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this
  * file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -17,7 +17,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  */
 
@@ -153,7 +153,7 @@ enum {
 	kSCNetworkReachabilityFlagsConnectionOnDemand	= 1<<5,	// __OSX_AVAILABLE_STARTING(__MAC_10_6,__IPHONE_3_0)
 	kSCNetworkReachabilityFlagsIsLocalAddress	= 1<<16,
 	kSCNetworkReachabilityFlagsIsDirect		= 1<<17,
-#if	TARGET_OS_IPHONE || TARGET_OS_EMSCRIPTEN
+#if	TARGET_OS_IPHONE
 	kSCNetworkReachabilityFlagsIsWWAN		= 1<<18,
 #endif	// TARGET_OS_IPHONE
 
