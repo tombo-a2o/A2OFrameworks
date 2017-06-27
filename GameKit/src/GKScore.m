0a27b90f68cd5c76e9191380f2486e87503d6223
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-27 19:14:08 +0900

    Add stubs

--- GameKit/src/GKScore.m
+++ GameKit/src/GKScore.m
@@ -1,8 +1,60 @@
 #import <GameKit/GKScore.h>
 
 @implementation GKScore
-- (instancetype)initWithCategory:(NSString *)category
+- (instancetype)initWithLeaderboardIdentifier:(NSString *)identifier
 {
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
     return nil;
 }
+
+- (instancetype)initWithLeaderboardIdentifier:(NSString *)identifier player:(GKPlayer *)player
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
++ (void)reportScores:(NSArray<GKScore *> *)scores withCompletionHandler:(void (^)(NSError *error))completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
++ (void)reportScores:(NSArray<GKScore *> *)scores withEligibleChallenges:(NSArray<GKChallenge *> *)challenges withCompletionHandler:(void (^)(NSError *error))completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (UIViewController *)challengeComposeControllerWithMessage:(NSString *)message players:(NSArray<GKPlayer *> *)players completionHandler:(GKChallengeComposeCompletionBlock)completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
+- (UIViewController *)challengeComposeControllerWithPlayers:(NSArray<NSString *> *)playerIDs message:(NSString *)message completionHandler:(GKChallengeComposeCompletionBlock)completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
+- (instancetype)initWithCategory:(NSString *)category
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
+- (instancetype)initWithLeaderboardIdentifier:(NSString *)identifier forPlayer:(NSString *)playerID
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+    return nil;
+}
+
+- (void)issueChallengeToPlayers:(NSArray<NSString *> *)playerIDs message:(NSString *)message
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
+- (void)reportScoreWithCompletionHandler:(void (^)(NSError *error))completionHandler
+{
+    NSLog(@"*** %s is not implemented", __FUNCTION__);
+}
+
 @end
