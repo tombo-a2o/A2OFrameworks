75d80520789d4fa4a0de84433ffd5e259ce8324d
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-27 19:13:57 +0900

    Remove assertion

--- Security/src/SecItem.m
+++ Security/src/SecItem.m
@@ -5,24 +5,26 @@
 OSStatus SecItemCopyMatching(CFDictionaryRef _query, CFTypeRef *_result)
 {
     NSDictionary *query = _query;
-    
-    // currently supports only kSecClass=kSecClassGenericPassword and kSecReturnAttributes=true 
+
+    NSLog(@"%s: %@", __FUNCTION__, query);
+
+    // currently supports only kSecClass=kSecClassGenericPassword and kSecReturnAttributes=true
     NSObject *secClass = [query objectForKey:kSecClass];
     assert(secClass == kSecClassGenericPassword);
     NSNumber *returnData = [query objectForKey:kSecReturnData];
     NSNumber *returnAttributes = [query objectForKey:kSecReturnAttributes];
     NSNumber *returnRef = [query objectForKey:kSecReturnRef];
     NSNumber *returnPersistentRef = [query objectForKey:kSecReturnPersistentRef];
-    assert(![returnData boolValue]);
-    assert([returnAttributes boolValue]);
-    assert(![returnRef boolValue]);
-    assert(![returnPersistentRef boolValue]);
-    
+    // assert(![returnData boolValue]);
+    // assert([returnAttributes boolValue]);
+    // assert(![returnRef boolValue]);
+    // assert(![returnPersistentRef boolValue]);
+
     NSString *service = [query objectForKey:kSecAttrService];
     NSString *account = [query objectForKey:kSecAttrAccount];
     NSLog(@"%s query: %@", __FUNCTION__, query);
-    
-    
+
+
     NSDictionary *result = [[NSDictionary alloc] init];
     *_result = result;
     return errSecItemNotFound;
@@ -31,11 +33,11 @@ OSStatus SecItemCopyMatching(CFDictionaryRef _query, CFTypeRef *_result)
 OSStatus SecItemAdd(CFDictionaryRef _attributes, CFTypeRef *result)
 {
     NSDictionary *attributes = _attributes;
-    
+
     // currently supports only kSecClass=kSecClassGenericPassword
     NSObject *secClass = [attributes objectForKey:kSecClass];
     assert(secClass == kSecClassGenericPassword);
-    
+
     NSString *service = [attributes objectForKey:kSecAttrService];
     NSString *account = [attributes objectForKey:kSecAttrAccount];
     NSLog(@"%s attributes: %@", __FUNCTION__, attributes);
